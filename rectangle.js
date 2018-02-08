ba373053cc49577e40c8c36bf3b655f9649d9aee
Carlos Calla <carlos.calla89@gmail.com>
2018-02-07 23:22:01 -0500

    prueba de callbacks

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,21 @@
-exports.perimeter =  (x, y) => (2*(x+y));
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(()=>
+        callback(new Error("Rectangle dimensions should be greater than zero:  x = "+ x + ",  and y = " + y),
+        null) ,
+        2000);
+        console.log();
+    }
+    else {
+        setTimeout(()=>
+        callback(null,
+        {
+            perimeter :  (x, y) => (2*(x+y)),
+            area : (x, y) => (x*y)
+        }) ,
+        2000);
+    }
+}
 
-exports.area = (x, y) => (x*y);
+
+
